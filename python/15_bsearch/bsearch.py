1ee70de54871a15116668a238815116009aaf171
aiorosxul <xuliangaioros@foxmail.com>
2019-01-17 22:14:44 +0800

    Update bsearch.py

--- python/15_bsearch/bsearch.py
+++ python/15_bsearch/bsearch.py
@@ -1,20 +1,18 @@
 list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-value = 1
-flag = 0
+value = 7
 
 
-def Bsearch(list, value, flag) -> int:
-    try:
-        mid = len(list) // 2
-        flag += mid
+def Bsearch(list, value, start=0, end=None) -> int:
+    end = len(list) if end is None else end
+    mid = start + (end - start) // 2
+    if start <= end:
         if value == list[mid]:
-            return flag
+            return mid
         elif value < list[mid]:
-            return Bsearch(list[:mid], value, flag)
+            return Bsearch(list, value, start=start, end=mid - 1)
         else:
-            return Bsearch(list[mid:], value, flag)
-    except IndexError:
-        print('Value not found!')
+            return Bsearch(list, value, start=mid + 1, end=end)
+    else:
+        print('Not found')
 
-
-print(Bsearch(list, value, flag))
+print(Bsearch(list, value))
