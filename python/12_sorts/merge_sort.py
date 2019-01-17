9820328186914bc18d4899b23364568b1fe0591a
aiorosxul <xuliangaioros@foxmail.com>
2019-01-17 21:26:30 +0800

    Update merge_sort.py

--- python/12_sorts/merge_sort.py
+++ python/12_sorts/merge_sort.py
@@ -1,65 +1,25 @@
-"""
-    Author: Wenru
-"""
-
-from typing import List
-
-
-def merge_sort(a: List[int]):
-    _merge_sort_between(a, 0, len(a) - 1)
-
-
-def _merge_sort_between(a: List[int], low: int, high: int):
-    # The indices are inclusive for both low and high.
-    if low < high:
-        mid = low + (high - low) // 2
-        _merge_sort_between(a, low, mid)
-        _merge_sort_between(a, mid + 1, high)
-        _merge(a, low, mid, high)
+def merge_sort(list):
+    if len(list) == 1:
+        return list
+    else:
+        mid = len(list) // 2
+        left = list[:mid]
+        right = list[mid:]
+        l1 = merge_sort(left)
+        r1 = merge_sort(right)
+        return merge(l1, r1)
 
 
-def _merge(a: List[int], low: int, mid: int, high: int):
-    # a[low:mid], a[mid+1, high] are sorted.
-    i, j = low, mid + 1
-    tmp = []
-    while i <= mid and j <= high:
-        if a[i] <= a[j]:
-            tmp.append(a[i])
-            i += 1
+def merge(left, right):
+    result = []
+    while len(left) > 0 and len(right) > 0:
+        if left[0] < right[0]:
+            result.append(left.pop(0))
         else:
-            tmp.append(a[j])
-            j += 1
-    start = i if i <= mid else j
-    end = mid if i <= mid else high
-    tmp.extend(a[start:end + 1])
-    a[low:high + 1] = tmp
+            result.append(right.pop(0))
+    result += left
+    result += right
+    return result
 
 
-def test_merge_sort():
-    a1 = [3, 5, 6, 7, 8]
-    merge_sort(a1)
-    assert a1 == [3, 5, 6, 7, 8]
-    a2 = [2, 2, 2, 2]
-    merge_sort(a2)
-    assert a2 == [2, 2, 2, 2]
-    a3 = [4, 3, 2, 1]
-    merge_sort(a3)
-    assert a3 == [1, 2, 3, 4]
-    a4 = [5, -1, 9, 3, 7, 8, 3, -2, 9]
-    merge_sort(a4)
-    assert a4 == [-2, -1, 3, 3, 5, 7, 8, 9, 9]
-
-
-if __name__ == "__main__":
-    a1 = [3, 5, 6, 7, 8]
-    a2 = [2, 2, 2, 2]
-    a3 = [4, 3, 2, 1]
-    a4 = [5, -1, 9, 3, 7, 8, 3, -2, 9]
-    merge_sort(a1)
-    print(a1)
-    merge_sort(a2)
-    print(a2)
-    merge_sort(a3)
-    print(a3)
-    merge_sort(a4)
-    print(a4)
+print(merge_sort(list))
